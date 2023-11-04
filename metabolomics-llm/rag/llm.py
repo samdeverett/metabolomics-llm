04b22cfa911efb06db2cc37a80fb31ffcfbd294f
samdeverett <samdeverett@gmail.com>
2023-11-04 18:11:37 -0400

    llm.py docs

--- metabolomics-llm/rag/llm.py
+++ metabolomics-llm/rag/llm.py
@@ -6,13 +6,11 @@ import transformers
 
 from langchain.llms.huggingface_pipeline import HuggingFacePipeline
 from torch import cuda, bfloat16
-from transformers.models.llama.modeling_llama import LlamaForCausalLM
-from transformers.models.llama.tokenization_llama_fast import LlamaTokenizerFast
+from transformers import PreTrainedModel, PreTrainedTokenizerBase
 from transformers.pipelines.text_generation import TextGenerationPipeline
-from typing import Dict, Any
 
 
-def init_llm(model_id: str, hf_auth_token: str) -> LlamaForCausalLM:
+def init_model(model_id: str, hf_auth_token: str) -> PreTrainedModel:
     """Initializes a pretrained Hugging Face model.
 
     Args:
@@ -51,8 +49,8 @@ def init_llm(model_id: str, hf_auth_token: str) -> LlamaForCausalLM:
     return model
 
 
-def init_tokenizer(model_id: str, hf_auth_token: str) -> LlamaTokenizerFast:
-    """Initializes a pretrained Hugging Face model.
+def init_tokenizer(model_id: str, hf_auth_token: str) -> PreTrainedTokenizerBase:
+    """Initializes a pretrained Hugging Face tokenizer.
 
     Args:
         model_id: The model id of a pretrained tokenizer hosted inside a model repo on huggingface.co.
@@ -68,13 +66,43 @@ def init_tokenizer(model_id: str, hf_auth_token: str) -> LlamaTokenizerFast:
 
 
 def init_text_generation_pipeline(
-        model: LlamaForCausalLM,
-        tokenizer: LlamaTokenizerFast,
-        model_kwargs: Dict[str, Any] = {}
+        model: PreTrainedModel,
+        tokenizer: PreTrainedTokenizerBase,
+        # GenerationConfig parameters (https://huggingface.co/docs/transformers/main_classes/text_generation#transformers.GenerationConfig)
+        max_new_tokens: int = 4096,
+        do_sample: bool = True,
+        temperature: float = 0.6,
+        top_p: float = 0.9,
+        repetition_penalty: float = 1.1
     ) -> TextGenerationPipeline:
-    pass
+    """Initializes a text generation pipeline for predicting the words that follow a prompt.
+
+    Args:
+        model: The model that will be used by the pipeline to make predictions.
+        tokenizer: The tokenizer that will be used by the pipeline to encode data for the model.
+        max_new_tokens: The maximum numbers of tokens to generate, ignoring the number of tokens in the prompt.
+        do_sample: Whether or not to use sampling ; use greedy decoding otherwise.
+        temperature: The value used to modulate the next token probabilities.
+        top_p: If set to float < 1, only the smallest set of most probable tokens with probabilities that add up to top_p or higher are kept for generation.
+        repetition_penalty: The parameter for repetition penalty. 1.0 means no penalty. See this paper for more details: https://arxiv.org/pdf/1909.05858.pdf.
+    """
+    text_generation_pipeline = transformers.pipeline(
+        task='text-generation',
+        model=model,
+        tokenizer=tokenizer,
+        max_new_tokens=max_new_tokens,
+        do_sample=do_sample,
+        temperature=temperature,
+        top_p=top_p,
+        repetition_penalty=repetition_penalty
+    )
+    return text_generation_pipeline
 
 def init_langchain_pipeline(pipeline: TextGenerationPipeline) -> HuggingFacePipeline:
+    """Initializes a LangChain model for a given Hugging Face TextGenerationPipeline.
+    
+    Args:
+        pipeline: Hugging Face TextGenerationPipeline to turn into a LangChain model.
+    """
     pipeline = HuggingFacePipeline(pipeline=pipeline)
     return pipeline
-git 
